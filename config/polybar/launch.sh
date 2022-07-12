4bef619e5e8f3b3181743545bebc2ef66a8609a9
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-07-11 23:53:44 -0500

    handle multi-monitor setup

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,14 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --config=$HOME/.config/polybar/config.ini --reload mybar &
+  done
+else
+  polybar --config=$HOME/.config/polybar/config.ini --reload mybar &
+fi
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 /usr/bin/polybar --config=$HOME/.config/polybar/config.ini mybar 2>&1 | tee -a /tmp/polybar1.log & disown
